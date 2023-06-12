518009bcda19e9c1bdd9ad8b4a64a64999d96359
Shubham Pachori <shubham.pachori@databricks.com>
2023-06-11 17:12:38 -0700

    adding pydantic validators

--- harness/config/SnapshotConfig.py
+++ harness/config/SnapshotConfig.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel,validator
 
 from harness.config.SourceConfig import SourceConfig
 from harness.config.TargetConfig import TargetConfig
@@ -17,3 +17,25 @@ class SnapshotConfig(BaseModel):
     validated: bool = False
     validation_date: Optional[datetime] = None
     validation_report: Optional[str] = None
+    
+    
+    @classmethod
+    @validator("target")
+    def valid_target(cls,value):
+        if value is None or isinstance(value,TargetConfig) == False:
+            raise ValueError("target provided is null or it's of incorrect datatype")
+        return value
+    
+    @classmethod
+    @validator("source")
+    def valid_source(cls,value):
+        if value is None or isinstance(value,SourceConfig) == False:
+            raise ValueError("source provided is null or it's of incorrect datatype")
+        return value
+    
+    @classmethod
+    @validator("validated")
+    def valid_validated(cls,value):
+        if value is None or isinstance(value,bool) == False:
+            raise ValueError("validated provided is null or it's of incorrect datatype")
+        return value
